b0a8b95eb8dc6b7c52264fd32feed74d454abf0b
Jimmy Smutek <jimmy@smutek.net>
2019-02-01 16:18:44 -0500

    Did it work?

--- labs/05-deploying/gatsby-config.js
+++ labs/05-deploying/gatsby-config.js
@@ -6,7 +6,7 @@ require(`dotenv`).config({
 module.exports = {
   siteMetadata: {
     // TODO: add your siteUrl
-    siteUrl: `https://your-site-name.netlify.com`,
+    siteUrl: `https://angry-liskov-131154.netlify.com`,
     title: `Gatsby Drupal Blog`,
     description: `An amazing blog built in the Gatsby + Drupal workshop at DrupalCamp 2019`,
     author: `@schaudustin`,
